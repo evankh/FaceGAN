511092ff3c90042a8aeeada1c48889fffc4a312e
evankh <evankarlhoffman@gmail.com>
2019-10-16 21:02:30 -0400

    Added image resizing script.

---
+++ resize.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+
+base = "../00000/"
+out_sizes = [4, 8, 16, 32, 64]
+in_folder = "128/"
+
+dirs = os.listdir(base + in_folder)
+
+for size in out_sizes:
+	out_folder = base + str(size) + "/"
+	if not os.path.exists(out_folder):
+		os.mkdir(out_folder)
+	for item in dirs:
+		if os.path.isfile(base + in_folder + item):
+			im = Image.open(base + in_folder + item)
+			imResize = im.resize((size, size), Image.BILINEAR)
+			imResize.save(out_folder + item, "PNG")
+			im.close()
+			imResize.close()
