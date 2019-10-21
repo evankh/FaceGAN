afc242b4a64ac93ab9010ab9f3a8d351e3e2e2c2
David Marquez <david.marquez@mymail.champlain.edu>
2019-10-21 12:25:05 -0400

    Update model.py

--- model.py
+++ model.py
@@ -12,28 +12,27 @@ from keras.optimizers import Adam
 from keras import backend as K
 import numpy as np
 import sys, glob
-#import cv2
 import os
 import pytest
 import argparse
 
-def generator_model(inputdim = 100, xdim = 8, ydim = 8):
+def generator(inputdim = 100, xdim = 8, ydim = 8):
     # xdim = 8, ydim = 8 results in prediction shape of (1, 3, 128, 128)
     model = Sequential()
     model.add(Dense(input_dim=inputdim, output_dim=1024*xdim*ydim))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Reshape( (1024, xdim, ydim), input_shape=(inputdim,) ) )
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(512, 5, 5, border_mode='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
+    model.add(Reshape( (128, xdim, ydim), input_shape=(inputdim,) ) )
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Convolution2D(256, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(128, 5, 5, border_mode='same'))
+    model.add(Convolution2D(512, 5, 5, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Convolution2D(1024, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
@@ -41,18 +40,18 @@ def generator_model(inputdim = 100, xdim = 8, ydim = 8):
     model.add(Activation('tanh'))
     return model
 
-def discriminator_model():
+def discriminator():
     model = Sequential()
-    model.add(Convolution2D(128, 5, 5, subsample=(2, 2), input_shape=(3, 128, 128), border_mode = 'same'))
-    model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(256, 5, 5, subsample=(2, 2), border_mode = 'same'))
+    model.add(Convolution2D(1024, 5, 5, subsample=(2, 2), input_shape=(3, 128, 128), border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
     model.add(Convolution2D(512, 5, 5, subsample=(2, 2), border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(1024, 5, 5, subsample=(2, 2), border_mode = 'same'))
+    model.add(Convolution2D(256, 5, 5, subsample=(2, 2), border_mode = 'same'))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(128, 5, 5, subsample=(2, 2), border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
     model.add(Flatten())
@@ -60,18 +59,7 @@ def discriminator_model():
     model.add(Activation('sigmoid'))
     return model
 
-
-def generator_containing_discriminator(generator, discriminator):
-    model = Sequential()
-    model.add(generator)
-    discriminator.trainable = False
-    model.add(discriminator)
-    #discriminator.trainable = False
-    return model
-
 def noise(size):
-    '''
-    Generates a 1-d vector of gaussian sampled random values
-    '''
+    #Generates a 1-d vector of gaussian sampled random values
     n = Variable(torch.randn(size, 100))
     return n
