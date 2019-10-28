1051b82dde4efc2fe046604d8f7e9f0381be3751
evankh <evankarlhoffman@gmail.com>
2019-10-28 17:12:16 -0400

    Removed magic numbers from code and added a few more comments for clarity.

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import numpy as np
 
 input_size = 64
 latent_size = 64
+initializer_shape = (4, 4, latent_size)
+starting_resolution = 4
 
 class NoiseLayer(tf.keras.layers.Layer):
         """Adds random zero-centered Gaussian noise vector with the
@@ -18,6 +20,8 @@ class NoiseLayer(tf.keras.layers.Layer):
                 self.scale = self.add_weight(shape=(1,),
                                               initializer=tf.keras.initializers.zeros,
                                               name="scaling_factor")
+                # Should it instead be per-channel scaling factors? I feel like it should
+                # And maybe per-channel standard deviations as well
         def compute_output_shape(self, input_shape):
                 return input_shape
         def call(self, inputs):
@@ -25,6 +29,7 @@ class NoiseLayer(tf.keras.layers.Layer):
 
 class ConstantLayer(tf.keras.layers.Layer):
         """Creates a set of learned constants to use as initial input.
+        Ignores any input given to it and only returns the learned weights, but must be given an input layer to match the size of.
         """
         def __init__(self, **kwargs):
                 super(ConstantLayer, self).__init__(**kwargs)
@@ -39,12 +44,16 @@ class ConstantLayer(tf.keras.layers.Layer):
                 return inputs - inputs + self.constant
 
 class AdaptiveInstanceNormalizationLayer(tf.keras.layers.Layer):
-        """Does the magic AdaIN transformation.
+        """Performs a learned AdaIN transformation.
+        Matches the mean and standard deviation of the input to the learned mean and standard deviation of the dataset.
+        Uses the input latent code as a "style" which controls factors in the generated image; the learned weights are
+        the effect each factor of the latent code has on each feature of the image.
         """
         def __init__(self, **kwargs):
                 super(AdaptiveInstanceNormalizationLayer, self).__init__(**kwargs)
         def build(self, input_shape):
                 # Learns a mapping from the latent space to per-channel Mean and StdDev
+                # This is NOT how this is supposed to work! Results in a network with over 1 Billion weights at 1024x1024!
                 assert len(input_shape) == 2
                 latent_shape, layer_shape = input_shape
                 self.stddev = self.add_weight(shape=latent_shape[1:] + layer_shape[1:],
@@ -59,9 +68,10 @@ class AdaptiveInstanceNormalizationLayer(tf.keras.layers.Layer):
         def call(self, inputs):
                 assert len(inputs) == 2
                 latent_code, layer = inputs
-                # Aligns Mean and StdDev of input to learned Mean and StdDev of "style"
-                normalized = (layer - K.mean(layer)) / K.std(layer)     # Is this doing it per-channel or per-layer?
-                return K.batch_dot(latent_code, K.expand_dims(self.stddev,0), axes=1) * normalized + K.batch_dot(latent_code, K.expand_dims(self.mean,0), axes=1)
+                normalized = (layer - K.mean(layer, axis=0)) / K.std(layer, axis=0)     # axis=0 is the batch, so it computes the mean and stddev of each sample separately, across the whole batch
+                new_stddev = K.batch_dot(latent_code, K.expand_dims(self.stddev,0), axes=1)
+                new_mean = K.batch_dot(latent_code, K.expand_dims(self.mean,0), axes=1)
+                return new_stddev * normalized + new_mean
 
 class Generator:
         """Keeps all necessary information for the generator in one place.
@@ -73,16 +83,16 @@ class Generator:
                 self.model = tf.keras.Model(inputs=self.inputs, outputs=self.synthesis)
                 self.model.compile(loss="mean_squared_error", optimizer="adam")
         def generate(self, noise):
-                return self.model.predict([noise, np.zeros((noise.shape[0], 4, 4, latent_size))])
+                return self.model.predict([noise, np.zeros((noise.shape[0],) + initializer_size)])
         def train_on_batch(self, x, y):
-                return self.model.train_on_batch([x, np.zeros((x.shape[0], 4, 4, latent_size))], y)
+                return self.model.train_on_batch([x, np.zeros((x.shape[0],) + initializer_size)], y)
 
 class Discriminator:
         """Keeps all necessary information for the discriminator in one place.
         """
         def __init__(self, classifier):
                 self.classifier = classifier
-                self.resolution = 4
+                self.resolution = starting_resolution
         def classify(self, image):
                 return K.argmax(self.classifier.predict(image))
 
@@ -100,15 +110,13 @@ mapping.add(tf.keras.layers.Dense(latent_size, activation="relu"))
 mapping.compile(loss="mean_squared_error", optimizer="adam")
 
 # Synthesis Network
-# Has to be rewritten using the Functional API so that the AdaIN layers can take multiple inputs
-# Although I'm quite certain there must be a way to make it work with Sequential.
-# Benefit of this is that is should be training mapping at the same time
+# Has to be written using the Functional API so that the AdaIN layers can take multiple inputs.
 latent_input = tf.keras.layers.Input(shape=(input_size,))
-ignore_input = tf.keras.layers.Input(shape=(4, 4, latent_size))
+ignore_input = tf.keras.layers.Input(shape=initializer_shape)
 synthesis = ConstantLayer()(ignore_input)
 synthesis = tf.keras.layers.Flatten()(synthesis)
 synthesis = tf.keras.layers.Dense(48)(synthesis)
-synthesis = tf.keras.layers.Reshape((4, 4, 3))(synthesis)
+synthesis = tf.keras.layers.Reshape((starting_resolution, starting_resolution, 3))(synthesis)   # 3 = R, G, B
 synthesis = NoiseLayer(1.0)(synthesis)
 synthesis = AdaptiveInstanceNormalizationLayer()([mapping(latent_input), synthesis])
 synthesis = tf.keras.layers.Conv2D(filters=3, kernel_size=3, padding="same")(synthesis)
@@ -119,9 +127,10 @@ generator = Generator(mapping, synthesis, [latent_input, ignore_input])
 
 # Discriminator Network
 classifier = tf.keras.Sequential()
-classifier.add(tf.keras.layers.Input((4, 4, 3)))
+classifier.add(tf.keras.layers.Input((starting_resolution, starting_resolution, 3)))    # 3 = R, G, B
 classifier.add(tf.keras.layers.Conv2D(filters=3, kernel_size=3, padding="same"))
 classifier.add(tf.keras.layers.Flatten())
+classifier.add(tf.keras.layers.Dense(8, activation="relu"))
 classifier.add(tf.keras.layers.Dense(2, activation="relu"))
 classifier.add(tf.keras.layers.Activation(tf.keras.activations.softmax))
 classifier.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
